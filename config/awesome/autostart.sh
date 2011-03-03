a0d1f4ea53f2b6c6ca2389a296726cb0d46f0c94
Michon van Dooren <michon1992@gmail.com>
2011-03-03 21:27:56 +0100

    Added my awesome stuff.

---
+++ config/awesome/autostart.sh
@@ -0,0 +1,38 @@
+# Restore the backgrounds.
+nitrogen --restore &
+
+# Stop here if the autorun file has been run before this session. Everything after this is "run-once", so to say.
+if ! [ -e ~/.config/awesome/doautostart ];
+then
+  exit 0;
+fi;
+rm ~/.config/awesome/doautostart
+
+# Screensaver/lock screen.
+xscreensaver &
+
+# Numlock.
+numlockx &
+
+# Volume control.
+#(sleep 30 && obmixer) &
+
+# Apply .Xresources.
+xrdb -merge ~/.Xresources &
+
+# Apply .Xmodmap
+(sleep 10 && xmodmap .Xmodmap) &
+
+# G15 keys.
+g15macro &
+
+# Autostart programs.
+DISPLAY=:0.0 pidgin & 
+DISPLAY=:0.0 transmission-gtk & 
+#DISPLAY=:0.0 keeprunning quodlibet &
+
+DISPLAY=:0.1 keeprunning xchat &
+#DISPLAY=:0.1 firefox &
+
+# Start playing quodlibet after 30 seconds.
+#(sleep 30 && quodlibet --play) &
