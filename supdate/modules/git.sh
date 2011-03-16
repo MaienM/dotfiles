de23fc26f2e6a4346f1d1146b954766b5653dafb
Michon van Dooren <michon1992@gmail.com>
2011-03-16 14:00:19 +0100

    Updated some of the supdate modules.

--- supdate/modules/git.sh
+++ supdate/modules/git.sh
@@ -1,13 +1,10 @@
 import 'versioncontrol'
 
 CMDS[check]='
-O=$(git status | head -n3) && 
-N=$(
-(
-  echo $O | grep behind ||
-  echo $O | grep diverged | grep -oE "[0-9]+ different"
-) | grep -oE [0-9]+ || echo 0)'
+T=$(git log --oneline HEAD..origin/master) &&
+N=$(echo $T | wc -l)'
 CMDS[refresh]='git fetch -q >> /dev/null'
 CMDS[download]='
+C=$(git log --oneline HEAD..origin/master | sed "s/^\(\S\+\)/  \\\\033[33m\1\\\\033[m/") &&
 T=$(git pull | grep changed | cut -c 2-) &&
 N=$(echo $T | grep -oE ^[0-9]+)'
--- supdate/modules/versioncontrol.sh
+++ supdate/modules/versioncontrol.sh
@@ -1,3 +1,3 @@
-TEXT[download]='result 
-               "Failed to download the updates for $NAME"
-               "Checked out $NAME: $T"'
+TEXT[download]='result \
+               "Failed to download the updates for $NAME" \
+               "Checked out $NAME: $T\n$C"'
