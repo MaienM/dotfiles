d1794204e4b9adf550f3aaeb7472941dc48bb890
Michon van Dooren <michon1992@gmail.com>
2011-03-19 12:10:23 +0100

    Fixed the git module for supdate.

--- supdate/modules/git.sh
+++ supdate/modules/git.sh
@@ -5,6 +5,6 @@ T=$(git log --oneline HEAD..origin/master) &&
 N=$(echo $T | wc -l)'
 CMDS[refresh]='git fetch -q >> /dev/null'
 CMDS[download]='
-C=$(git log --oneline HEAD..origin/master | sed "s/^\(\S\+\)/  \\\\033[33m\1\\\\033[m/") &&
+C=$(git log --oneline HEAD..origin/master) &&
 T=$(git pull | grep changed | cut -c 2-) &&
 N=$(echo $T | grep -oE ^[0-9]+)'
