43269e349b21774b78a540b95c291c3e62ba9077
Michon van Dooren <michon1992@gmail.com>
2011-04-14 10:47:29 +0200

    INTR: Changed apply.sh a bunch. It will now detect all problems/missing links and attempt to fix them automatically whenever possible. It now also uses the file .virtualdirs, which indicates which dirs are to be linked. For example, I have the file config/awesome/rc.lua which I want to link, but since ~/.config and ~/.config/awesome contain other files which I don't want to version, I'll add those two directories to .virualdirs. This will prevent it from trying to link config to ~/.config.

--- apply.sh
+++ apply.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
+# An alias for ln to direct it to a file.
 function ln()
 {
   echo ln $@ >> cmds
 }
+function cmd()
+{
+  echo $@ >> cmds
+}
 
+# Ask a question.
 function ask()
 {
   D=${1//[abcdefghijklmnopqrstuvwxyz]} # [a-z] didn't work as it also matched
                                        # uppercase letters.
-  if [[ ${#D} -ne 1 ]]
+  if [[ ${#D} -gt 1 ]]
   then
-    echo "There either are multiple or no default specified in this ask sequence: $1."
+    echo "There are multiple defaults specified in this ask sequence: $1."
     return
   fi
 
@@ -23,9 +29,41 @@ function ask()
 
   if [[ -z $A ]]
   then
-    A=$D
+    if [[ ${#D} -eq 1 ]]
+    then
+      A=$D
+    else
+      ask "$1"
+    fi
   fi
+  echo 
   A=${A,,}
+}
+
+# Compare two files/folders.
+function inode()
+{
+  echo $(stat -L -c%i "$1")
+}
+function cmp()
+{
+  [[ $(inode "$1") == $(inode "$2") ]] && echo 1
+}
+
+# Compare the age of two files/folders.
+function lastmod()
+{
+  echo $(stat -L -c%Z "$1")
+}
+
+
+# Traverse into a directory.
+function traverse()
+{
+  for FILE in "$1/"*[^~]
+  do
+    process "$FILE"
+  done
 }
 
 function process()
@@ -37,41 +75,82 @@ function process()
   then
     continue
   fi
+ 
+  if [[ -d $OF ]]
+  then
+    D=$(grep -cE "^$OF$" .virtualdirs)
+  fi
 
   if [[ -e $NF ]]
   then
-    if [[ -d $NF ]]
+    if [[ -d $NF && -d $OF ]]
     then
-      if [[ $(ls -1 "$NF"/*[^~] | wc -l) -gt 0 ]]
+      if [[ $D -gt 0 ]]
       then
-        echo "The directory $NF already exists, and it's not empty. What do you want to do?"
-        echo "[o]verwrite it, [T]raverse into it."
-        ask 'oT'
-        if [[ $A == 'o' ]]
+        traverse "$OF"
+      else
+        if [[ $(cmp "$OF" "$NF") -eq 1 ]]
         then
-          ln -sf "$OF" "$NF"
-        elif [[ $A == 't' ]]
+          :
+        else
+          echo "Directories $OF and $NF aren't the same. We don't do anything about this"
+          echo "automatically (yet), but you should look at it."
+        fi
+      fi
+    elif [[ -f $NF && -f $OF ]]
+    then
+      if [[ $(cmp "$OF" "$NF") -eq 1 ]]
+      then
+        :
+      elif [[ -z $(diff -q "$OF" "$NF") ]]
+      then
+        ln -f "$OF" "$NF"
+      else
+        echo "The file $NF already exists."
+        if [[ $(lastmod "$NF") -lt $(lastmod "$OF") ]]
         then
-          for FILE in $OF/*[^~]
-          do
-            process "$FILE"
-          done
+          echo "$NF has been changed more recently than $OF."
+        else
+          echo "$OF has been changed more recently than $NF."
+        fi
+
+        echo "What do you want to do?"
+        echo "[m]ove $NF to $OF and create a link, [r]emove $NF and link $OF to $NF, show [D]iff, [i]gnore conflict."
+        ask 'mrDi'
+          
+        if [[ $A == 'd' ]]
+        then
+          vimdiff "$OF" "$NF"
+          echo "What do you want to do?"
+          echo "[m]ove $NF to $OF and create a link, [r]emove $NF and link $OF to $NF, [i]gnore conflict."
+          ask 'mri'
+        fi
+
+        if [[ $A == 'm' ]]
+        then
+          cmd mv "$NF" "$OF"
+          ln "$OF" "$NF"
+        elif [[ $A == 'r' ]]
+        then
+          ln -f "$OF" "$NF"
         fi
       fi
     else
-      echo "The file $NF already exists. Overwite it? [y/N]"
-      ask 'yN'
-      if [[ $A == 'y' ]]
-      then
-        ln -f "$OF" "$NF"
-      fi
+      echo "The types (file or directory) of $OF and $NF don't match. Not sure what to do with this."
     fi
-  elif [[ -d $OF ]]
-  then
-    ln -s "$OF" "$NF"
-  elif [[ ! -x $OF ]]
-  then
-    ln "$OF" "$NF"
+  else
+    if [[ -d $OF ]]
+    then
+      if [[ $D -gt 0 ]]
+      then
+        cmd mkdir "$NF"
+        traverse "$OF"
+      else
+        ln -s "$OF" "$NF"
+      fi
+    else
+      ln "$OF" "$NF"
+    fi
   fi
 }
 
