a105a25dc315943df2b8498f8e768d4e9bc6bb9b
Michon van Dooren <michon1992@gmail.com>
2015-04-05 12:46:27 +0200

    Updated powerline. Setup powerline for IPython.

--- config/powerline/segments/maienm/shell.py
+++ config/powerline/segments/maienm/shell.py
@@ -13,23 +13,26 @@ def is_shell(pl, pid):
 @requires_segment_info
 def shell_level(pl, segment_info, min_level=0, only_count_shells=True, require_parent_shell=True):
 	"""
-        Returns the current shell level.
+    Returns the current shell level.
 
 	By default this is $SHLVL.
 
-	:param int min_level:
+	:param int min_level
 		The minimum required level.
-        :param bool only_count_shells
-                Whether to only count parent processes that are shells.
+
+    :param bool only_count_shells
+        Whether to only count parent processes that are shells.
+
 		This means that it will recursively count the parent processes
 		until it encounters one that doesn't end in 'sh', or til it has
-		checked $SHLVL parents. 
+		checked $SHLVL parents.
 		This count will then be used as the shell level.
 		This means the output may not be equal to $SHLVL
-        :param bool require_parent_shell:
-                Whether the parent process has to be a shell.
+
+    :param bool require_parent_shell
+        Whether the parent process has to be a shell.
 		This means the parent process has to end in 'sh'.
-        """
+    """
 	lvl = int(segment_info['environ'].get('SHLVL', 0))
 	pid = int(segment_info.get('client_id', os.getppid()))
 	ppid, _ = get_data(pl, pid)
