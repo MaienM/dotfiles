d99811a9a22cdddbd787566ed4eab277949823db
Michon van Dooren <michon1992@gmail.com>
2015-09-07 10:37:42 +0200

    ZSH: cd-ing into a directory with a .venv file will now automatically activate that virtual environment.

---
+++ aliases/program-specific/cd.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+# Automatically activate Git projects' virtual environments based on the
+# directory name of the project. Virtual environment name can be overridden
+# by placing a .venv file in the project root with a virtualenv name in it
+function workon_cwd {
+    # Check that this is a Git repo
+    GIT_DIR=`git rev-parse --git-dir 2> /dev/null`
+    if [[ $? == 0 ]]; then
+        # Find the repo root and check for virtualenv name override
+        GIT_DIR=`\cd $GIT_DIR; pwd`
+        PROJECT_ROOT=`dirname "$GIT_DIR"`
+        ENV_NAME=`basename "$PROJECT_ROOT"`
+        if [ -f "$PROJECT_ROOT/.venv" ]; then
+            ENV_NAME=`cat "$PROJECT_ROOT/.venv"`
+        fi
+        # Activate the environment only if it is not already active
+        if [ "$VIRTUAL_ENV" != "$WORKON_HOME/$ENV_NAME" ]; then
+            if [ -e "$WORKON_HOME/$ENV_NAME/bin/activate" ]; then
+                workon "$ENV_NAME" && export CD_VIRTUAL_ENV="$ENV_NAME"
+            fi
+        fi
+    elif [[ $CD_VIRTUAL_ENV ]]; then
+        # We've just left the repo, deactivate the environment
+        # Note: this only happens if the virtualenv was activated automatically
+        deactivate && unset CD_VIRTUAL_ENV
+    fi
+}
+
+# The combined cd function.
+function combined_cd {
+	builtin cd "$@" && workon_cwd
+}
+
+alias cd='combined_cd'
