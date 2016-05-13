33cbd1386cce854523bf4711b65690ce0be83a69
Michon van Dooren <michon1992@gmail.com>
2016-05-13 10:07:55 +0200

    Added docker aliases

---
+++ aliases/aliases/docker.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+alias docker='sudo -E docker'
+alias dockps='docker ps --format="table {{.Names}}\t{{.ID}}\t{{.Image}}\t{{.Status}}"'
+
+dockbuild() {
+    name="docker.waxd.nl/${1:-$(basename $(realpath .))}"
+    docker build -t "$name:$(git rev-parse HEAD)" -t "$name" .
+}
+
+dockpush() {
+    docker push docker.waxd.nl/${1:-$(basename $(realpath .))}
+}
+
+dockbuildpush() {
+    dockbuild "$1" && dockpush "$1"
+}
+
+docker_remove_untagged() {
+    docker rmi $(docker images | grep '^<none>' | awk '{print $3}')
+}
