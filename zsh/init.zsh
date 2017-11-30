dc06e04bed0c2295e521c1a2292869f859ffd6d4
Michon van Dooren <michon1992@gmail.com>
2017-11-30 22:22:56 +0000

    Added the history substring search plugin to ZSH

--- zsh/init.zsh
+++ zsh/init.zsh
@@ -1,6 +1,7 @@
 # Load plugins
 source ~/.zsh/bundle/powerlevel9k/powerlevel9k.zsh-theme
 source ~/.zsh/bundle/syntax-highlighting/zsh-syntax-highlighting.zsh
+source ~/.zsh/bundle/history-substring-search/zsh-history-substring-search.zsh
 
 # Load the rc files responsible for the rest of the initialisation.
 source ~/.zsh/rc/**/*.zsh
