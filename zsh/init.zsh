6afc6db05d55a3b820848b912b97555a51679381
Michon van Dooren <michon1992@gmail.com>
2017-11-30 22:21:51 +0000

    Added syntax highlighting to ZSH

--- zsh/init.zsh
+++ zsh/init.zsh
@@ -1,5 +1,6 @@
 # Load plugins
 source ~/.zsh/bundle/powerlevel9k/powerlevel9k.zsh-theme
+source ~/.zsh/bundle/syntax-highlighting/zsh-syntax-highlighting.zsh
 
 # Load the rc files responsible for the rest of the initialisation.
 source ~/.zsh/rc/**/*.zsh
