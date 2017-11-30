8ef30255e461dd6894874e4588f2a89ebbbbc74c
Michon van Dooren <michon1992@gmail.com>
2017-11-30 23:27:34 +0000

    Added the autosuggestions plugin to ZSH

--- zsh/init.zsh
+++ zsh/init.zsh
@@ -1,6 +1,13 @@
+# Version check
+version() {
+	echo "$@" | sed 's/[0-9]\+/00&/g' | sed 's/[0-9]\+\([0-9]\{3\}\)/\1/g' | tr -d '.'
+}
+
 # Load plugins
 source ~/.zsh/bundle/powerlevel9k/powerlevel9k.zsh-theme
-source ~/.zsh/bundle/syntax-highlighting/zsh-syntax-highlighting.zsh
+[ $(version $ZSH_VERSION) -ge $(version 5.0.8) ] && \
+	source ~/.zsh/bundle/autosuggestions/zsh-autosuggestions.zsh
+source ~/.zsh/bundle/syntax-highlighting/zsh-syntax-highlighting.zsh # Should be behind most other plugins
 source ~/.zsh/bundle/history-substring-search/zsh-history-substring-search.zsh
 
 # Load the rc files responsible for the rest of the initialisation.
