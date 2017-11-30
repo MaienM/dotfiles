ac7fb3f77c71be27ae091c5416d45ced140b7581
Michon van Dooren <michon1992@gmail.com>
2017-11-30 22:11:28 +0000

    Setup the ZSH prompt using powerlevel9k

---
+++ zsh/rc/theme.zsh
@@ -0,0 +1,19 @@
+source ~/.zsh/bundle/powerlevel9k/powerlevel9k.zsh-theme
+
+# Segments
+POWERLEVEL9K_LEFT_PROMPT_ELEMENT=(context dir rbenv vcs)
+POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status command_execution_time root_indicator background_jobs history time)
+
+# Multiline with empty line above
+POWERLEVEL9K_PROMPT_ADD_NEWLINE=true
+POWERLEVEL9K_PROMPT_ON_NEWLINE=true
+POWERLEVEL9K_MULTILINE_FIRST_PROMPT_PREFIX=""
+POWERLEVEL9K_MULTILINE_LAST_PROMPT_PREFIX="%{%B%F{yellow}%K{black}%} $user_symbol%{%b%f%k%F{black}%} %{%f%}"
+
+# Status of last command if needed
+POWERLEVEL9K_STATUS_OK=false
+
+# Short path + write status
+POWERLEVEL9K_SHORTEN_DIR_LENGTH=3
+POWERLEVEL9K_SHORTEN_STRATEGY='truncate_from_right'
+POWERLEVEL9K_DIR_SHOW_WRITABLE=true
