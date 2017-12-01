4cab60da8352bebe2cad25955e427b1881b68a37
Michon van Dooren <michon1992@gmail.com>
2017-12-01 00:01:10 +0000

    Sorted the zshrc, and removed the version check now that I have no systems that need it

--- zsh/init.zsh
+++ zsh/init.zsh
@@ -1,12 +1,7 @@
-# Version check
-version() {
-	echo "$@" | sed 's/[0-9]\+/00&/g' | sed 's/[0-9]\+\([0-9]\{3\}\)/\1/g' | tr -d '.'
-}
-
 # Load plugins
+source ~/.zsh/bundle/autosuggestions/zsh-autosuggestions.zsh
+source ~/.zsh/bundle/docker-compose/docker-compose.plugin.zsh
 source ~/.zsh/bundle/powerlevel9k/powerlevel9k.zsh-theme
-[ $(version $ZSH_VERSION) -ge $(version 5.0.8) ] && \
-	source ~/.zsh/bundle/autosuggestions/zsh-autosuggestions.zsh
 source ~/.zsh/bundle/prezto-history/init.zsh
 source ~/.zsh/bundle/syntax-highlighting/zsh-syntax-highlighting.zsh # Should be behind most other plugins
 source ~/.zsh/bundle/history-substring-search/zsh-history-substring-search.zsh
