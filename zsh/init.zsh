0a02ff419c5cde7e26e5364aa8e4b5da80d25ff0
Michon van Dooren <michon1992@gmail.com>
2017-12-01 01:06:46 +0000

    Replaced syntax-highlighting with fast-syntax-highlighting

--- zsh/init.zsh
+++ zsh/init.zsh
@@ -2,7 +2,7 @@
 source ~/.zsh/bundle/autosuggestions/zsh-autosuggestions.zsh
 source ~/.zsh/bundle/powerlevel9k/powerlevel9k.zsh-theme
 source ~/.zsh/bundle/prezto-history/init.zsh
-source ~/.zsh/bundle/syntax-highlighting/zsh-syntax-highlighting.zsh # Should be behind most other plugins
+source ~/.zsh/bundle/syntax-highlighting/fast-syntax-highlighting.plugin.zsh # Should be behind most other plugins
 source ~/.zsh/bundle/history-substring-search/zsh-history-substring-search.zsh
 
 # Load the rc files responsible for the rest of the initialisation.
