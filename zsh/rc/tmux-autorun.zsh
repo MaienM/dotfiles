e6c4013fab1569c71ad561fb903e480c49171044
Michon van Dooren <michon1992@gmail.com>
2017-12-02 15:21:49 +0000

    Always run ZSH inside of TMUX

---
+++ zsh/rc/tmux-autorun.zsh
@@ -0,0 +1,22 @@
+# Stop if tmux is not available, or we're in a session nested in something
+if (
+	[ ! $+commands[tmux] ] ||
+	[ -n "$TMUX" ] ||
+	[ -n "$EMACS" ] || 
+	[ -n "$VIM" ] ||
+	[ -n "$INSIDE_EMACS" ]
+); then
+	return 1
+fi
+
+# Start the tmux server if needed
+tmux start-server
+
+# Create a base session if there are no sessions
+if ! tmux has-session 2> /dev/null; then
+	tmux new-session -d -s "base" &> /dev/null
+	tmux set-option -t "base" destroy-unattached off &> /dev/null
+fi
+
+# Attach to the last session used (which will be the newly created session if none existed previously)
+exec tmux attach-session
