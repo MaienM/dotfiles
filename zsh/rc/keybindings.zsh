c352d338e53b5dbf14c839aebd7d9131a2817f70
Michon van Dooren <michon1992@gmail.com>
2017-12-04 13:41:15 +0100

    Removed a bunch of keybinding escape codes that are not used

--- zsh/rc/keybindings.zsh
+++ zsh/rc/keybindings.zsh
@@ -5,15 +5,12 @@ key_info=(
 	'Control'         '\C-'
 	'ControlLeft'     '\e[1;5D \e[5D \e\e[D \eOd'
 	'ControlRight'    '\e[1;5C \e[5C \e\e[C \eOc'
-	'ControlPageUp'   '\e[5;5~'
-	'ControlPageDown' '\e[6;5~'
 	'Escape'          '\e'
-	'Meta'            '\M-'
 	'Up'              "$terminfo[kcuu1]"
 	'Left'            "$terminfo[kcub1]"
 	'Down'            "$terminfo[kcud1]"
 	'Right'           "$terminfo[kcuf1]"
-	'BackTab'         "$terminfo[kcbt]"
+	'BackTab'         "$terminfo[kcbt]" # Shift-tab
 )
 
 # Warn on missing keys
