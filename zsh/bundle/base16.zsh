079416c0ac70d30192fd21fcff448c5b02f0f871
Michon van Dooren <michon1992@gmail.com>
2017-12-06 01:17:10 +0100

    Added an alias for the base16 colortest script

--- zsh/bundle/base16.zsh
+++ zsh/bundle/base16.zsh
@@ -1,2 +1,3 @@
 BASE16_SHELL=$HOME/.zsh/bundle/base16
 [ -n "$PS1" ] && [ -s $BASE16_SHELL/profile_helper.sh ] && eval "$($BASE16_SHELL/profile_helper.sh)"
+alias base16-colortest="$BASE16_SHELL/colortest"
