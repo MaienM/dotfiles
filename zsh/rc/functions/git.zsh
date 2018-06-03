6a15d1b0818e971c4d8ae5073f32e9af481f8075
Michon van Dooren <michon1992@gmail.com>
2018-06-03 21:48:36 +0200

    Added an FZF pipeline for git branches

--- zsh/rc/functions/git.zsh
+++ zsh/rc/functions/git.zsh
@@ -74,3 +74,38 @@ _fzf_pipeline_git_commit_preview() {
     git show --name-status "$1"
 }
 
+# Branches
+_fzf_pipeline_git_branch_source() {
+    # Get all branches, local and remote, with their full name.
+    # Filter out remote branches that have the same name and HEAD as another branch (local or remote).
+    local __ branch columns name commit
+    columns=$(($COLUMNS / 5))
+    git branch --all "$@" \
+    | grep -v HEAD \
+    | sed 's/^[[:space:]*]*//' \
+    | while read branch; do
+        # Output format: branch_full remote_name branch_name commit_hash
+        echo -n ${branch#remotes/}
+        if [[ "$branch" == "remotes/"* ]]; then
+            echo -n ' '${${branch#remotes/}%/*}
+        else
+            echo -n ' !!!!!local'
+        fi
+        echo -n ' '${branch#remotes/*/}
+        echo -n ' '$(git rev-parse "$branch")
+        echo
+    done \
+    | sort --key=2 \
+    | sort --key=4 --stable \
+    | sort --key=3 --stable \
+    | uniq --skip-fields=2 \
+    | sort --key=2 --stable \
+    | while read branch __ __ __; do
+        echo -n $branch
+        echo -n ' '${fg[yellow]}${branch[0,$columns]}$reset_color
+        echo -n ' | '$(git log --pretty=format:%s "$branch" --max-count=1)
+        echo
+    done \
+    | column --table --separator='|' --output-separator=''
+}
+alias _fzf_pipeline_git_branch_preview='_fzf_pipeline_git_commit_preview'
