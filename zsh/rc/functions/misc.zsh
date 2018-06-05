04e8b35498e6dfb835030738c10b805112f58e53
Michon van Dooren <michon1992@gmail.com>
2018-06-05 13:51:05 +0200

    Improved resolve_alias\n\nThere is no more risk of setting aliases, and it correctly combines all arguments

--- zsh/rc/functions/misc.zsh
+++ zsh/rc/functions/misc.zsh
@@ -6,18 +6,15 @@ fi
 
 # Get the definition of an alias
 resolve_alias() {
-    # If the input looks like an alias definition, abort to prevent setting an alias accidentally
-    if [[ "$@" == *"="* ]]; then
-        echo "$@"
-        return
-    fi
+    local args name output retval
 
-    local output retval
-    output=$(alias "$@")
+    args=(${(z)@})
+    name=${args[1]}
+    output=$(which "$name")
     retval=$?
-    if [[ $retval -eq 0 ]]; then
+    if [[ $retval -eq 0 ]] && [[ $output == "$name: aliased to "* ]]; then
         # Got an alias definition back
-        resolve_alias ${output#$@=}
+        resolve_alias ${output#$name: aliased to }  "${args[2,-1]}"
     else
         # The original input wasn't an alias, so return that
         echo "$@"
