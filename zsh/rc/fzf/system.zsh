68879b3122f6c190a7cbf957321a43dedd22a298
Michon van Dooren <michon1992@gmail.com>
2018-06-14 22:56:24 +0200

    Added an FZF pipeline/preset for system commands

---
+++ zsh/rc/fzf/system.zsh
@@ -0,0 +1,25 @@
+# Commands
+_fzf_pipeline_commands_source() {
+    apropos --sections 1,8 '' | sed 's/^\(\S*\)\s*(\S*)\s*-\s*/\1 \1 /'
+}
+_fzf_pipeline_commands_preview() {
+    man "${(Q)1}" \
+    | awk '
+        BEGIN { found = 0; }
+        {
+            if (/^DESCRIPTION$/) { found = 1; next }
+            if (/^[A-Z]/) { found = 0 }
+            if (found) { print }
+        }
+    ' \
+    | head -n-1 \
+    | sed 's/^\s*//'
+}
+_fzf_pipeline_commands_target() {
+    echo ${(Q)1}
+}
+
+# Presets
+alias _fzf_preset_commands='_fzf_config_add commands'
+
+_fzf_register_preset "commands" "Commands" "commands"
