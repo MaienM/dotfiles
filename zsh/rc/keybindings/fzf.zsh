126a67dd59830127f4c52e5edfb1a44a0b681539
Michon van Dooren <michon1992@gmail.com>
2018-06-21 10:28:04 +0200

    Fixed some (rendering) issues with the fzf keybind

--- zsh/rc/keybindings/fzf.zsh
+++ zsh/rc/keybindings/fzf.zsh
@@ -6,8 +6,14 @@ fi
 fzf_complete_preset() {
     local prefix result
     prefix=${${(zA)LBUFFER}[-1]}
-    result=$(fzf_run_preset "$prefix" --height='30%' --preview-window='right') || return 1
-    LBUFFER="${LBUFFER%$prefix}$result"
+    result=$(
+        fzf_run_preset \
+            "$prefix" \
+            --height='30%' \
+            --preview-window='right' \
+    )
+    [[ -n "$result" ]] && LBUFFER="${LBUFFER%$prefix}$result"
+    zle redisplay
 }
 zle -N fzf_complete_preset
 for key in "$key_info[Control]"{e,E}; do
