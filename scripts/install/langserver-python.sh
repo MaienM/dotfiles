f283e116be46a955b8eae789f82dc7318f929e20
Michon van Dooren <michon1992@gmail.com>
2018-07-24 12:18:21 +0000

    Fixed a bug in the langserver-python script

--- scripts/install/langserver-python.sh
+++ scripts/install/langserver-python.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # Python
-if which pyenv &> /dev/null
-  && (pyenv version | grep -v system) &> /dev/null
+if which pyenv &> /dev/null \
+  && (pyenv version | grep -v system) &> /dev/null \
   && python -c "import sys; sys.exit(sys.version_info < (3, 2))";
 then
   pip install "python-language-server[rope,pyflakes,mccabe,pycodestyle,pydocstyle]" pyls-mypy
