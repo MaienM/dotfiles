2c654ba95c2e9f1e4a14e1c0cd333a07aedbb301
Michon van Dooren <michon1992@gmail.com>
2018-07-30 15:39:43 +0200

    Added diff-so-fancy

--- scripts/install/misc.sh
+++ scripts/install/misc.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
 bash ~/.fzf/install --bin
+
+wget https://raw.githubusercontent.com/so-fancy/diff-so-fancy/master/third_party/build_fatpack/diff-so-fancy \
+	-O ~/.local/bin/diff-so-fancy
+chmod +x ~/.local/bin/diff-so-fancy
