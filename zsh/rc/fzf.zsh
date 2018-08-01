9757099ad7224714b3c0427685c992160bfc912a
Michon van Dooren <michon1992@gmail.com>
2018-08-01 15:01:43 +0200

    Added fzf defaults to make the selected items more visible

--- zsh/rc/fzf.zsh
+++ zsh/rc/fzf.zsh
@@ -8,5 +8,8 @@ for file in ~/.zsh/rc/fzf/**/*.zsh; do
 	source "$file";
 done
 
+export FZF_DEFAULT_OPTS=""
 # Change the multi-select direction, so you can just repeatedly hit tab to select everything
-export FZF_DEFAULT_OPTS="--bind tab:toggle-out,shift-tab:toggle-in"
+export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS --bind tab:toggle-out,shift-tab:toggle-in"
+# Make the multi-select markers more distinct
+export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS --color=dark,marker:230"
