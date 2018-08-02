0bfe72f05aadd1b4e25c8bcf4abd31cf5f910b72
Michon van Dooren <michon1992@gmail.com>
2018-08-02 07:16:22 +0000

    Don't install mypy with the python langserver for now

--- scripts/install/langserver-python.sh
+++ scripts/install/langserver-python.sh
@@ -5,7 +5,7 @@ if which pyenv &> /dev/null \
   && (pyenv version | grep -v system) &> /dev/null \
   && python -c "import sys; sys.exit(sys.version_info < (3, 2))";
 then
-  pip install "python-language-server[rope,pyflakes,mccabe,pycodestyle,pydocstyle]" pyls-mypy
+  pip install "python-language-server[rope,pyflakes,mccabe,pycodestyle,pydocstyle]"
 else
   echo "Python 3.2 or newer must be installed through pyenv"
   exit 1
