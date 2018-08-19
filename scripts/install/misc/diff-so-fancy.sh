de346037a4144f9b407e479ff26116b4e7f68649
Michon van Dooren <michon1992@gmail.com>
2018-08-19 12:43:24 +0200

    Include diff-so-fancy in opt
    Previously it was installed with one of the install scripts. This way
    the version used is pinned, like most other dependencies.

--- scripts/install/misc/diff-so-fancy.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-
-wget https://raw.githubusercontent.com/so-fancy/diff-so-fancy/master/third_party/build_fatpack/diff-so-fancy \
-	-O ~/.local/bin/diff-so-fancy
-chmod +x ~/.local/bin/diff-so-fancy
