5207dccdd46e1e9b47b0ef3c2bfd274130ac2035
Michon van Dooren <michon1992@gmail.com>
2018-09-06 23:10:15 +0200

    Correct file using an old name of a rename function

--- zsh/bundle/fzf.zsh
+++ zsh/bundle/fzf.zsh
@@ -1,7 +1,7 @@
 # Not actually in the bundle folder, as it is used outside of zsh as well
 FZF_PATH="$HOME/.fzf"
 
-append-to-path "$FZF_PATH/bin"
+append_to_path "$FZF_PATH/bin"
 [[ $- == *i* ]] && source "$FZF_PATH/shell/completion.zsh" 2> /dev/null
 source "$FZF_PATH/shell/key-bindings.zsh"
 
