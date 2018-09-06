b1bcae3989c39fba86068119f4556c15485bb277
Michon van Dooren <michon1992@gmail.com>
2018-09-06 23:05:48 +0200

    Use the base16 theme for fzf

--- zsh/rc/fzf.zsh
+++ zsh/rc/fzf.zsh
@@ -8,8 +8,10 @@ for file in ~/.zsh/rc/fzf/**/*.zsh; do
 	source "$file";
 done
 
+# Load the base16 theme
 export FZF_DEFAULT_OPTS=""
+[ -f $HOME/.base16_theme_fzf ] && source $HOME/.base16_theme_fzf
+# Remove the background color, to make transparency work
+export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS --color=bg+:-1,bg:-1"
 # Change the multi-select direction, so you can just repeatedly hit tab to select everything
 export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS --bind tab:toggle-out,shift-tab:toggle-in"
-# Make the multi-select markers more distinct
-export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS --color=dark,marker:230"
