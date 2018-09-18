740ca24c93b23ced14130cafdd3e1ea9b40e0e33
Michon van Dooren <michon1992@gmail.com>
2018-09-18 17:53:07 +0200

    Fixed a style error in the xresources hook

--- zsh/bundle/base16-hooks/xresources.sh
+++ zsh/bundle/base16-hooks/xresources.sh
@@ -3,7 +3,7 @@
 set -o errexit
 
 # Install the colors.
-cp ~/.zsh/bundle/base16-hooks/base16-xresources/xresources/base16-$BASE16_THEME-256.Xresources ~/.Xresources.d/colors
+cp "$HOME/.zsh/bundle/base16-hooks/base16-xresources/xresources/base16-$BASE16_THEME-256.Xresources" ~/.Xresources.d/colors
 
 # Apply the Xresources.
 xrdb -merge ~/.Xresources
