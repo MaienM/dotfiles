c0dfe8cb0fe5ff1a7975505a67699a045924eb94
Michon van Dooren <michon1992@gmail.com>
2018-11-09 14:47:46 +0100

    Optimized move-workspace-and-stay

--- config/i3/move-workspace-to-output-stay.sh
+++ config/i3/move-workspace-to-output-stay.sh
@@ -5,8 +5,4 @@ set -o errexit -o pipefail
 target="$1"
 output="$(i3-msg -t get_workspaces | jq 'map(select(.focused))[0].output')"
 
-i3-msg "move workspace to output $target"
-
-new_workspace="$(i3-msg -t get_workspaces | jq "map(select(.visible and .output == $output))[0].name")"
-
-i3-msg "workspace $new_workspace"
+i3-msg "move workspace to output $target; focus output $output"
