0073d9047edb0c8c6aa8aeb3d61c78497b146e41
Michon van Dooren <michon1992@gmail.com>
2018-11-21 14:01:25 +0100

    Improved the kubed command
    It now includes a list of available shortcuts.
    
    The chosen command is now pushed to the history, making it easy to
    re-run without having to go through the interactive menu every time.

--- zsh/rc/functions/kubernetes.zsh
+++ zsh/rc/functions/kubernetes.zsh
@@ -1,3 +1,8 @@
 kubed() {
-    kubectl describe $(fzf_run_preset "kubernetes:all")
+    history_add_and_run $(fzf_run_preset "kubernetes:all" \
+        --header=$'Usage:\nAlt + d: describe\nAlt + l: logs (+ Ctrl for follow)' \
+        --bind="alt-d:abort+execute(echo kubectl describe {2} >&4)" \
+        --bind="alt-l:abort+execute(echo kubectl logs {2} >&4)" \
+        --bind="ctrl-alt-l:abort+execute(echo kubectl logs -f {2} >&4)" \
+    4>&1)
 }
