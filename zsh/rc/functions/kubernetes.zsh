a24a9f91c348b341d2e0d89b1243a5fae8a22639
Michon van Dooren <michon1992@gmail.com>
2018-11-21 15:05:50 +0100

    Fix pressing enter in kubed

--- zsh/rc/functions/kubernetes.zsh
+++ zsh/rc/functions/kubernetes.zsh
@@ -1,8 +1,13 @@
 kubed() {
-    history_add_and_run $(fzf_run_preset "kubernetes:all" \
+    local cmd
+    cmd=($(fzf_run_preset "kubernetes:all" \
         --header=$'Usage:\nAlt + d: describe\nAlt + l: logs (+ Ctrl for follow)' \
+        --bind="enter:abort" \
         --bind="alt-d:abort+execute(echo kubectl describe {2} >&4)" \
         --bind="alt-l:abort+execute(echo kubectl logs {2} >&4)" \
         --bind="ctrl-alt-l:abort+execute(echo kubectl logs -f {2} >&4)" \
-    4>&1)
+    4>&1))
+    if [[ -n "${cmd[@]}" ]]; then
+        history_add_and_run ${cmd[@]}
+    fi
 }
