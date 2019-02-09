5ef1215d2912b306aaf731ec048b487cc51fc9ec
Michon van Dooren <michon1992@gmail.com>
2019-02-09 23:42:20 +0100

    Added a script for luaenv

---
+++ scripts/install/devenvs/luaenv.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+
+git clone https://github.com/cehoffman/luaenv.git ~/.luaenv
+git clone git://github.com/cehoffman/lua-build.git ~/.luaenv/plugins/lua-build
+git clone https://github.com/xpol/luaenv-luarocks.git ~/.luaenv/plugins/luaenv-luarocks
