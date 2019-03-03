b13fb26dc9a98d774bf73143c867f31eedc50f26
Michon van Dooren <michon1992@gmail.com>
2019-03-03 16:41:02 +0100

    Allow installing the python langserver on 2.7

--- scripts/install/langservers/langserver-python.sh
+++ scripts/install/langservers/langserver-python.sh
@@ -3,10 +3,10 @@
 # Python
 if command -v pyenv > /dev/null 2>&1 \
   && (pyenv version | grep -v system) > /dev/null 2>&1 \
-  && python -c "import sys; sys.exit(sys.version_info < (3, 2))";
+  && python -c "import sys; sys.exit(sys.version_info < 2.7 or (sys.version_info >= (3,) and sys.version_info < (3, 2)))";
 then
   pip install "python-language-server[rope,pyflakes,mccabe,pycodestyle,pydocstyle]"
 else
-  echo "Python 3.2 or newer must be installed through pyenv"
+  echo "Python 2.7+ or 3.2+ must be installed through pyenv"
   exit 1
 fi
