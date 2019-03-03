31749aeaa00260296f695c629b81487d14550b77
Michon van Dooren <michon1992@gmail.com>
2019-03-03 16:39:07 +0100

    Use the base16 colors in polybar

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -11,6 +11,13 @@ set -a
 source nerdfonts_icons_all
 set +a
 
+# Make the colors available as environment variables
+eval "$(xrdb -query | grep '^*color' | while read -r line; do
+	name="$(echo "$line" | cut -c2- | cut -d: -f1)"
+	color="$(echo "$line" | cut -d: -f2 | tr -d '[:space:]')"
+	echo export "$name='$color'"
+done)"
+
 # Launch bars for all screens
 mapfile -t monitors < <(xrandr --listmonitors | cut -d' ' -f6 | grep -E '^.+$')
 MONITOR="${monitors[0]}" polybar primary &
