70a0076b4889820afc59b9049832732e3d391161
Michon van Dooren <michon1992@gmail.com>
2019-03-03 16:40:41 +0100

    Added the pyqt-build plugin for pyenv

--- scripts/install/devenvs/pyenv.sh
+++ scripts/install/devenvs/pyenv.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
 
 curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
+[ -d "$HOME/.pyenv/plugins/pyenv-pyqt-build" ] || git clone https://github.com/montefra/pyenv-pyqt-build "$HOME/.pyenv/plugins/pyenv-pyqt-build"
 
