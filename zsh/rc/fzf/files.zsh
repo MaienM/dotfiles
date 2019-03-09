23ecd2c96a53688648c240b8271d533f51c63c91
Michon van Dooren <michon1992@gmail.com>
2019-03-09 12:01:14 +0100

    Added a fancy-print version of find and ls
    Also used these in the fzf file preview for directories.

--- zsh/rc/fzf/files.zsh
+++ zsh/rc/fzf/files.zsh
@@ -2,7 +2,7 @@
 _fzf_util_inspect_fs_item() {
     fn="$1"
     if [ -d "$fn" ]; then
-        ls -hl "$fn"
+        fls "$fn"
     elif [ -f "$fn" ]; then
         pygmentize -g -O "full,style=$BASE16_THEME" "$fn"
     else
