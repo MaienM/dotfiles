39bd9d7fe6bc50c858ba3e0cd60ca5cc977d174e
Michon van Dooren <michon1992@gmail.com>
2019-04-01 19:21:32 +0200

    Fixed the langserver-python install script

--- scripts/install/langservers/langserver-python.sh
+++ scripts/install/langservers/langserver-python.sh
@@ -3,7 +3,7 @@
 # Python
 if command -v pyenv > /dev/null 2>&1 \
   && (pyenv version | grep -v system) > /dev/null 2>&1 \
-  && python -c "import sys; sys.exit(sys.version_info < 2.7 or (sys.version_info >= (3,) and sys.version_info < (3, 2)))";
+  && python -c "import sys; sys.exit(sys.version_info < (2, 7) or (sys.version_info >= (3,) and sys.version_info < (3, 2)))";
 then
   pip install "python-language-server[rope,pyflakes,mccabe,pycodestyle,pydocstyle]"
 else
