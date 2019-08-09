abf875403aa2592dd0008a5867d50473acae664e
Michon van Dooren <michon1992@gmail.com>
2019-08-09 19:19:59 +0200

    Add a trailing space a fzf preset expansion

--- zsh/rc/keybindings/fzf.zsh
+++ zsh/rc/keybindings/fzf.zsh
@@ -4,19 +4,19 @@ fi
 
 # Easily complete using presets
 fzf_complete_preset() {
-    local prefix result
-    if [[ "$LBUFFER" == "${LBUFFER%[$IFS]}" ]]; then
-        # Doesn't end in ifs, so we're treat the currently typed word as a preexisting filter
-        prefix=${${(zA)LBUFFER}[-1]}
-    fi
-    result=$(
-        fzf_run_preset \
-            "$prefix" \
-            --height='30%' \
-            --preview-window='right' \
-    )
-    [[ -n "$result" ]] && LBUFFER="${LBUFFER%$prefix}$result"
-    zle redisplay
+	local prefix result
+	if [[ "$LBUFFER" == "${LBUFFER%[$IFS]}" ]]; then
+		# Doesn't end in ifs, so we'll treat the currently typed word as a preexisting filter
+		prefix=${${(zA)LBUFFER}[-1]}
+	fi
+	result=$(
+		fzf_run_preset \
+			"$prefix" \
+			--height='30%' \
+			--preview-window='right' \
+	)
+	[[ -n "$result" ]] && LBUFFER="${LBUFFER%$prefix}$result "
+	zle redisplay
 }
 zle -N fzf_complete_preset
 for key in "$key_info[Control]"{e,E}; do
