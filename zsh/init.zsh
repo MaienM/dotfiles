47bf4e9aa89ec0f70c6046d1b79ab7a2f9bca1da
Michon van Dooren <michon1992@gmail.com>
2019-08-12 19:58:19 +0200

    Reordered zsh init
    The extra files (zsh/rc/**) are now loaded before the
    syntax-highlighting plugin, which makes it possible to do more things in
    these files.

--- zsh/init.zsh
+++ zsh/init.zsh
@@ -1,19 +1,17 @@
-# Source profile
 source ~/.profile
 
-# Load version checking function, for version guards in the plugins
+# Load version checking function, for version guards in the plugins.
 autoload -Uz is-at-least
 
-# Load plugins
 source ~/.zsh/bundle/autosuggestions.zsh
 source ~/.zsh/bundle/base16.zsh
 source ~/.zsh/bundle/fzf.zsh
 source ~/.zsh/bundle/powerlevel10k/powerlevel10k.zsh-theme
 source ~/.zsh/bundle/prezto-history/init.zsh
-source ~/.zsh/bundle/syntax-highlighting.zsh # Should be behind most other plugins
-source ~/.zsh/bundle/history-substring-search.zsh
 
-# Load the rc files responsible for the rest of the initialisation.
 for file in ~/.zsh/rc/*.zsh; do
 	source "$file"
 done
+
+source ~/.zsh/bundle/syntax-highlighting.zsh # Wraps zle widgets, should be as late as possible.
+source ~/.zsh/bundle/history-substring-search.zsh # Must be after syntax highlighting.
