cd506064a761feb96ff726c389bdb567cd6acdb1
Michon van Dooren <michon1992@gmail.com>
2019-08-18 16:44:04 +0200

    Undid change in fzf keybind that broke things

--- zsh/rc/keybindings/fzf.zsh
+++ zsh/rc/keybindings/fzf.zsh
@@ -5,7 +5,7 @@ fi
 # Easily complete using presets
 fzf_complete_preset() {
 	local prefix result
-	if [[ "$LBUFFER" != *"$IFS" ]]; then
+	if [[ "$LBUFFER" == "${LBUFFER%[$IFS]}" ]]; then
 		# Doesn't end in ifs, so we'll treat the currently typed word as a preexisting filter
 		prefix=${${(zA)LBUFFER}[-1]}
 	fi
