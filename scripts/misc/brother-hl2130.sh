6739d79da56b912b00a2ac756300741b6cabeee4
Michon van Dooren <michon1992@gmail.com>
2019-09-22 21:45:02 +0200

    Minor (auto) changes

--- scripts/misc/brother-hl2130.sh
+++ scripts/misc/brother-hl2130.sh
@@ -4,4 +4,4 @@
 pikaur -S brother-hl2130
 
 # Add printer
-sudo lpadmin -p Brother_HL-2130 -E -v 'lpd://192.168.1.1/LPRServer' -m "HL2130.ppd"
+sudo lpadmin -p Brother_HL-2130 -E -v 'lpd://192.168.2.254/LPRServer' -m "HL2130.ppd"
