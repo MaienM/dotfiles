fc4b5e2da59a0e5eca245a645b6c0ca54136c67c
Michon van Dooren <michon1992@gmail.com>
2019-09-30 20:47:57 +0200

    tmux fzf switcher custom commands & format prefix

--- tmux/scripts/switcher.sh
+++ tmux/scripts/switcher.sh
@@ -2,27 +2,35 @@
 
 switcher_window_name='__switcher_window__'
 
-case "$1" in
+item="$1"
+command="${2:-tmux switch-client -t %%}"
+prompt="$prompt"
+prefix="$prefix"
+
+case "$item" in
 	sessions)
 		choose_tree_flag="-s"
-		list_command='list-sessions -F "#S #S (#{session_windows} windows)"'
+		visible_format="$prefix#S (#{session_windows} windows)"
+		list_command="list-sessions -F '#S $visible_format'"
 		child_command='list-windows -F "#S:#I #S:#W"'
 	;;
 
 	windows)
 		choose_tree_flag="-w"
-		list_command='list-windows -a -F "#S:#I #S: #W (#{window_panes} panes)"'
+		visible_format="$prefix#S: #W (#{window_panes} panes)"
+		list_command="list-windows -a -F '#S:#I $visible_format'"
 		child_command='list-panes -F "#S:#I.#P #S:#W.#P"'
 	;;
 
 	panes)
 		choose_tree_flag=
-		list_command='list-panes -a -F "#S:#I.#P #S: #W: #P (#T)"'
+		visible_format="$prefix#S: #W: #P (#T)"
+		list_command="list-panes -a -F '#S:#I.#P $visible_format'"
 		child_command='display-message -p -F "#S:#I.#P #S:#W.#P"'
 	;;
 
 	*)
-		[ -n "$1" ] && echo >&2 "Unknown type '$1'."
+		[ -n "$item" ] && echo >&2 "Unknown type '$item'."
 		echo >&2 'Please pass a valid type to switch between. Options: sessions, windows, panes.'
 		exit 1
 	;;
@@ -35,6 +43,7 @@ if command -v fzf > /dev/null 2>&1; then
 		| fzf \
 			--reverse \
 			--with-nth=2.. \
+			${prompt:+--prompt=\"$prompt\"} \
 			--preview-window='down:65%' \
 			--preview='
 				tmux $child_command -t {1} \
@@ -42,9 +51,9 @@ if command -v fzf > /dev/null 2>&1; then
 				| xargs -d\"\\n\" $HOME/.tmux/scripts/render-preview.sh \
 			' \
 		| cut -d' ' -f1 \
-		| xargs -r tmux switch-client -t
+		| xargs -r -I '%%' $command
 	"
 else
-	tmux choose-tree -Z "$choose_tree_flag"
+	tmux choose-tree -Z "$choose_tree_flag" -F "$visible_format" "$command"
 fi
 
