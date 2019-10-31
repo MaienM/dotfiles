e03f0e70783c0e4a6082075d92a400db4d9e7401
Michon van Dooren <michon1992@gmail.com>
2019-10-31 17:02:14 +0100

    Handle named workspaces in run-for-layer
    Fixes #24.

--- config/i3/run-for-layer.sh
+++ config/i3/run-for-layer.sh
@@ -1,11 +1,19 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 set -o errexit -o pipefail
 
 command_teplate="$1"
-source_workspace="$(i3-msg -t get_workspaces | jq 'map(select(.focused))[0].num')"
-target_workspace="$(((source_workspace + 10) % 20))"
-command="$(echo "$command_teplate" | sed "s/__source__/$source_workspace/g; s/__target__/$target_workspace/g")"
 
+declare -A workspaces
+while read -r num; read -r name; do
+	workspaces[$num]="$name"
+done < <(grep "set \$workspace" ~/.config/i3/config | cut -c15- | sed 's/\s/\n/')
+
+source_workspace="$(i3-msg -t get_workspaces | jq 'map(select(.focused))[0].num')"
+
+target_workspace="$(((source_workspace + 10) % 20))"
+target_workspace="${workspaces[$target_workspace]:-$target_workspace}"
+
+command="$(echo "$command_teplate" | sed "s/__source__/$source_workspace/g; s/__target__/$target_workspace/g")"
 eval "$command"
 
