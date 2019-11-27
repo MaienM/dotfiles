7857d6f7126e7b58e94d2585707836568e378dfc
Michon van Dooren <michon1992@gmail.com>
2019-11-27 20:03:40 +0100

    Load base16 colors in polybar script

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -8,8 +8,13 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Load the icons
 set -a
+# shellcheck disable=SC1091
 source nerdfonts_icons_all
 set +a
+
+# Make the colors available as environment variables
+# shellcheck disable=SC1090
+source ~/.profile.d/base16
 
 # Launch bars for all screens
 mapfile -t monitors < <(xrandr --listmonitors | cut -d' ' -f6 | grep -E '^.+$')
