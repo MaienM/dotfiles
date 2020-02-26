7b70e2dd13f44dc67d7086c3506d62389d4c7aad
Michon van Dooren <michon1992@gmail.com>
2020-02-26 09:27:37 +0100

    Replace tmux buffer pipelines with rg based ones

--- zsh/rc/fzf/tmux-buffer.zsh
+++ zsh/rc/fzf/tmux-buffer.zsh
@@ -1,13 +1,24 @@
-if ! command -v tmux &> /dev/null; then
+if ! command -v tmux &> /dev/null || ! command -v rg &> /dev/null; then
 	return
 fi
 
-_fzf_pipeline_tmux_buffer_urls_source() {
-	tmux capture-pane -pJ -S-500 | extract-url --list | tac | while read -r url; do
-		echo "${(q)url} $url"
-	done
+# Wrapper around the rg results pipeline that performs them on the current tmux buffer instead of a file.
+
+_fzf_pipeline_rg_tmux_source() {
+	file="$(mktemp)"
+	tmux capture-pane -pJe -t "$TMUX_PANE" -S-999 >! "$file"
+	_fzf_pipeline_rg_file_source "$file" "$@"
+}
+alias _fzf_pipeline_rg_tmux_preview='_fzf_pipeline_rg_file_preview'
+alias _fzf_pipeline_rg_tmux_target='_fzf_pipeline_rg_file_target'
+
+# Pipelines based on capturing specific types of data from the current tmux buffer.
+
+_add_fzf_pipeline_tmux_regex() {
+	alias _fzf_pipeline_rg_tmux_$1_source="_fzf_pipeline_rg_tmux_source '$2'"
+	alias _fzf_pipeline_rg_tmux_$1_preview='_fzf_pipeline_rg_tmux_preview'
+	alias _fzf_pipeline_rg_tmux_$1_target='_fzf_pipeline_rg_tmux_target'
+	alias _fzf_preset_tmux_$1="_fzf_config_add rg_tmux_$1"
+	_fzf_register_preset "tmux_$1" "$3 in current tmux buffer" "tmux:$4"
 }
 
-alias _fzf_preset_tmux_buffer_urls='_fzf_config_add tmux_buffer_urls'
-
-_fzf_register_preset "tmux_buffer_urls" "Tmux buffer urls" "tmux:buffer:urls"
