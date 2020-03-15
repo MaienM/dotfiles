6982a3732f7ae6a2ab6910074a7a6630a78d26f2
Michon van Dooren <michon1992@gmail.com>
2020-03-15 14:50:43 +0100

    Let serve-stuff setup a ruby environment if needed
    Also removed the prompts for installs, it now just installs what it
    needs automatically.

---
+++ scripts/install/misc/ruby-utils.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -o errexit -o pipefail
+
+cd ~/.dotfiles
+
+# shellcheck source=../../../profile.d/asdf
+source profile.d/asdf
+# shellcheck source=_utils.sh
+source scripts/install/misc/_utils.sh
+
+echo ">>> Checking dependencies."
+asdf_guard
+asdf_plugin_add ruby
+
+echo ">>> Installing latest ruby."
+asdf_install_latest_version ruby
+echo "Using ruby $version."
+ln -s "$HOME/.asdf/installs/ruby/$version" "$HOME/.asdf/installs/ruby/utils"
+
+echo ">>> Installing utilities."
+ASDF_RUBY_VERSION=utils asdf exec gem install asciidoctor
+ASDF_RUBY_VERSION=utils asdf exec gem install facets pusher listen webrick commonmarker
+asdf reshim ruby
+
