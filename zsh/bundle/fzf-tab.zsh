17a724f31575c61ad862342d2e67b0cc70589230
Michon van Dooren <me@maienm.com>
2020-07-10 08:48:45 +0200

    Only preview tab completion if it makes sense

--- zsh/bundle/fzf-tab.zsh
+++ zsh/bundle/fzf-tab.zsh
@@ -1,4 +1,5 @@
 source ~/.zsh/bundle/fzf-tab/fzf-tab.plugin.zsh
 source ~/.zsh/rc/fzf.early.zsh
 
-zstyle ':fzf-tab:*' extra-opts --preview 'preview {2}' $FZF_BIND_OPTS
+zstyle ':fzf-tab:*' extra-opts $FZF_BIND_OPTS
+zstyle ':fzf-tab:complete:*:argument-rest' extra-opts $FZF_BIND_OPTS --preview 'preview {2}'
