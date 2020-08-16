4a1ad47311e0ccbfbfce928f944b644595ac624e
Michon van Dooren <me@maienm.com>
2020-08-16 15:48:57 +0200

    Fixed a bug in histbd autosuggestions
    The problem was that commands that have been executed in multiple
    folders would be grouped together, resulting in them only appearing as a
    suggestion for one these folders, not for the rest.

--- zsh/rc/autosuggestions.zsh
+++ zsh/rc/autosuggestions.zsh
@@ -7,7 +7,7 @@ _zsh_autosuggest_strategy_histdb_top() {
 			LEFT JOIN commands ON history.command_id = commands.rowid
 			LEFT JOIN places ON history.place_id = places.rowid
 		WHERE commands.argv LIKE '$(sql_escape $1)%'
-		GROUP BY commands.argv
+		GROUP BY commands.argv, places.dir
 		ORDER BY places.dir != '$(sql_escape $PWD)', COUNT(*) DESC LIMIT 1
 	END
    suggestion=$(_histdb_query "$query")
