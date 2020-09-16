3024036067ced9728430409393b7e33a00d8a511
Michon van Dooren <me@maienm.com>
2020-09-16 14:12:07 +0200

    Simplified histdb autosuggestion code

--- zsh/rc/autosuggestions.zsh
+++ zsh/rc/autosuggestions.zsh
@@ -1,7 +1,7 @@
 # Use histdb to determine the autosuggestions based on previous commands in the current directory.
 _zsh_autosuggest_strategy_histdb_top() {
    local query
-	read -r -d '' query <<-END
+   suggestion="$(_histdb_query "
 		SELECT commands.argv
 		FROM history
 			LEFT JOIN commands ON history.command_id = commands.rowid
@@ -9,7 +9,6 @@ _zsh_autosuggest_strategy_histdb_top() {
 		WHERE commands.argv LIKE '$(sql_escape $1)%'
 		GROUP BY commands.argv, places.dir
 		ORDER BY places.dir != '$(sql_escape $PWD)', COUNT(*) DESC LIMIT 1
-	END
-   suggestion=$(_histdb_query "$query")
+	")"
 }
 ZSH_AUTOSUGGEST_STRATEGY=histdb_top
