e60c7b2c38b560a76570dcb8aff1830ca7c4a209
Michon van Dooren <me@maienm.com>
2020-12-06 12:48:28 +0100

    Update fzf-tab settings

--- zsh/bundle/fzf-tab.zsh
+++ zsh/bundle/fzf-tab.zsh
@@ -1,13 +1,5 @@
 source ~/.zsh/bundle/fzf-tab/fzf-tab.plugin.zsh
 source ~/.zsh/rc/fzf.early.zsh
 
-# Helper snippet from the fzf-tab readme to get the full path of the entry that is being previewed.
-local extract="
-	local in=\${\${\"\$(<{f})\"%\$'\0'*}#*\$'\0'}
-	local -A ctxt=(\"\${(@ps:\2:)CTXT}\")
-	local realpath=\${ctxt[IPREFIX]}\${ctxt[hpre]}\$in
-	realpath=\${(Qe)~realpath}
-"
-
-zstyle ':fzf-tab:*' extra-opts $FZF_BIND_OPTS
-zstyle ':fzf-tab:complete:*:argument-rest' extra-opts $FZF_BIND_OPTS --preview "$extract; "'preview "$realpath"'
+zstyle ':fzf-tab:*' fzf-flags $FZF_BIND_OPTS
+zstyle ':fzf-tab:complete:*:argument-rest' fzf-preview 'preview "$realpath"'
