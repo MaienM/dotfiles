be914ad7cc57d0e57ce9cad5141711d9eff25e72
Michon van Dooren <me@maienm.com>
2020-12-06 20:40:51 +0100

    Detect available network interfaces for polybar

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -18,6 +18,14 @@ set +a
 # shellcheck disable=SC1090
 source ~/.profile.d/base16
 
+# Get the names of the available physical network interfaces.
+mapfile -t eths < <(find /sys/class/net -type l -not -lname '*virtual*' -not -name 'w*' -printf '%f\n')
+mapfile -t wlans < <(find /sys/class/net -type l -not -lname '*virtual*' -name 'w*' -printf '%f\n')
+for i in {0..2}; do
+	declare -x "eth$i=${eths[i]}"
+	declare -x "wlan$i=${wlans[i]}"
+done
+
 # Launch bars for all screens
 mapfile -t monitors < <(xrandr --listmonitors | cut -d' ' -f6 | grep -E '^.+$')
 MONITOR="${monitors[0]}" polybar primary &
