33d1583fd906bdf402737637ecfb8dfbd3039f5a
Michon van Dooren <me@maienm.com>
2020-12-06 12:48:48 +0100

    Use tmux popup in fzf-tab if available

--- zsh/bundle/fzf-tab.zsh
+++ zsh/bundle/fzf-tab.zsh
@@ -1,5 +1,9 @@
 source ~/.zsh/bundle/fzf-tab/fzf-tab.plugin.zsh
 source ~/.zsh/rc/fzf.early.zsh
 
+if [ -n "$TMUX" ] && [ "$TMUX_SUPPORT_POPUP" = 1 ]; then
+	zstyle ':fzf-tab:*' fzf-command ftb-tmux-popup
+fi
+zstyle ':fzf-tab:*' popup-pad 50 1
 zstyle ':fzf-tab:*' fzf-flags $FZF_BIND_OPTS
 zstyle ':fzf-tab:complete:*:argument-rest' fzf-preview 'preview "$realpath"'
