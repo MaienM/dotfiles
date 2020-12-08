2adcc35c1251b215bdf3caaab8f751b6a94af599
Michon van Dooren <me@maienm.com>
2020-12-08 14:03:49 +0100

    Refresh update segment when pacman/pikaur finishes

--- config/polybar/scripts/updates.sh
+++ config/polybar/scripts/updates.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env sh
 
 arch=$(checkupdates | wc -l)
-aur=$(pikaur -Qua --devel --needed 2> /dev/null | wc -l)
+aur=$(pikaur -Qua 2> /dev/null | wc -l)
 
 if [ "$arch" -gt 0 ] || [ "$aur" -gt 0 ]; then
 	echo "$arch/$aur"
+else
+	echo
 fi
