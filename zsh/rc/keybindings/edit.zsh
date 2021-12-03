9c7062c50445f10ecb0b67ba7b729f62e6de675b
Michon van Dooren <me@maienm.com>
2021-12-03 16:52:25 +0100

    ZSH edit bind uses last command if BUFFER is empty

--- zsh/rc/keybindings/edit.zsh
+++ zsh/rc/keybindings/edit.zsh
@@ -5,6 +5,14 @@ fi
 autoload edit-command-line
 zle -N edit-command-line
 
+function edit-command-line-last {
+	# If nothing has been typed, use the last command
+	[[ -z $BUFFER ]] && zle up-history
+	zle edit-command-line
+}
+
+zle -N edit-command-line-last
+
 # Edit the current command in $EDITOR.
-bindkey "$key_info[Escape]i" edit-command-line
+bindkey "$key_info[Escape]i" edit-command-line-last
 
