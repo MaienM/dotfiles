b41690aa8090aae333a0fce51bb6b98f9fe8e200
Michon van Dooren <me@maienm.com>
2022-05-10 17:14:35 +0200

    Add script for new printer

---
+++ scripts/misc/brother-HL-L2350DW.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+# Install driver from aur
+pikaur -S --needed brother-hll2350dw
+
+# Add printer
+sudo lpadmin -x Brother_HL_L2350DW 
+sudo lpadmin -p Brother_HL_L2350DW -E -v 'lpd://192.168.2.254/LPRServer' -m "brother-HLL2350DW-cups-en.ppd"
+
+echo "To set printer as default run 'lpoptions -d Brother_HL_L2350DW'."
