c47bf3dd113cea76ebc268d9b0c0df2d3388e175
Michon van Dooren <me@maienm.com>
2022-09-14 10:25:35 +0200

    Automate updating completion files from commands

--- zsh/rc/completions.zsh
+++ zsh/rc/completions.zsh
@@ -1,5 +1,29 @@
+declare -A generated
+generated[ferium]="ferium complete zsh"
+generated[kubectl]="kubectl completion zsh"
+generated[poetry]="poetry completions zsh"
+
+fn="/tmp/$USER-zsh-completion-last-regen"
+regen=false
+if ! [ -f "$fn" ] || [ $(($(cat "$fn") + (12 * 60 * 60))) -lt "$(date +'%s')" ]; then
+	regen=true
+	date +'%s' > "$fn"
+fi
+
+for cmd gencmd in "${(kv)generated[@]}"; do
+	if ! [[ $commands[$cmd] ]]; then
+		continue
+	fi
+
+	fn="$HOME/.zsh/rc/completions/generated/_$cmd"
+	if $regen || ! [ -f "$fn" ]; then
+		${(z)gencmd} > "$fn"
+	fi
+done
+
 fpath=(
 	$HOME/.zsh/rc/completions
+	$HOME/.zsh/rc/completions/generated
 	$HOME/.asdf/completions
 	$HOME/.zsh/bundle/completion-gradle
 	$HOME/.zsh/bundle/completions/src
@@ -8,7 +32,3 @@ fpath=(
 
 autoload -Uz compinit
 compinit
-
-if [[ $commands[kubectl] ]]; then
-	source <(kubectl completion zsh)
-fi
