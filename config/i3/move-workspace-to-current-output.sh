54899ea30924e2c3bac9b327939cf43bef46648d
Michon van Dooren <me@maienm.com>
2022-12-09 13:43:16 +0100

    Multi-arg move-workspace-to-current-output.sh

--- config/i3/move-workspace-to-current-output.sh
+++ config/i3/move-workspace-to-current-output.sh
@@ -2,8 +2,9 @@
 
 set -o errexit -o pipefail
 
-workspace="$1"
 output="$(i3-msg -t get_workspaces | jq 'map(select(.focused))[0].output')"
 
-i3-msg "workspace $workspace; move workspace to output $output"
+for workspace in "$@"; do
+	i3-msg "workspace $workspace; move workspace to output $output"
+done
 
