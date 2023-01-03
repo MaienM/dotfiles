4064825af402d15fe1c487a4193ac5127222e9d6
Michon van Dooren <me@maienm.com>
2023-01-03 23:45:30 +0100

    Autoformat files using LSP

--- vim/rc/plugins/lsp-config.lua
+++ vim/rc/plugins/lsp-config.lua
@@ -20,6 +20,13 @@ local on_attach = function(client, bufnr)
 		vim.lsp.buf.format { async = true }
 	end, bufopts)
 	vim.keymap.set('n', '<Leader>aa', vim.lsp.buf.code_action, bufopts)
+
+	-- Autoformatting.
+	vim.api.nvim_create_autocmd('BufWritePre', {
+		callback = function()
+			vim.lsp.buf.format { async = false }
+		end,
+	})
 end
 
 -- General server setup.
