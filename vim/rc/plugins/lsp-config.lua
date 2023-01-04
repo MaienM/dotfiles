290a5c2033fafe1110eca383a8cf9bca69524f0e
Michon van Dooren <me@maienm.com>
2023-01-05 00:21:19 +0100

    Avoid excess restarts of sumneko on vimrc changes

--- vim/rc/plugins/lsp-config.lua
+++ vim/rc/plugins/lsp-config.lua
@@ -1,3 +1,5 @@
+local lspconfig = require('lspconfig')
+
 -- [D]iagnostics mappings.
 local opts = { noremap = true, silent = true }
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
@@ -28,14 +30,36 @@ local on_attach = function(client, bufnr)
 	})
 end
 
+-- Calling setup for a server may trigger a restart/reload even if nothing actually changed. This means that whenever we
+-- change anything in our vimrc the sumneko server is restarted, which is... obnoxious. To prevent this lets be smart
+-- about it and only re-run setup if something actually changed.
+vim.g.vimrc_lsp_config_setup_last_configs = vim.g.vimrc_lsp_config_setup_last_configs or {}
+local function setup(name, settings)
+	-- Every load will result in a new instance of the function, regardless of whether anything actually changed, so
+	-- ignore it. It would be nice to be smarter about this somehow, as this will result in changes to the on_attach
+	-- function not being picked up on reloads.
+	local on_attach = settings.on_attach
+	settings.on_attach = nil
+
+	local last_configs = vim.g.vimrc_lsp_config_setup_last_configs
+	if vim.deep_equal(settings, last_configs[name]) then
+		return
+	end
+
+	settings.on_attach = on_attach
+	last_configs[name] = settings
+	vim.g.vimrc_lsp_config_setup_last_configs = last_configs
+
+	lspconfig[name].setup(settings)
+end
+
 -- General server setup.
-local lspconfig = require('lspconfig')
 local common_setup_data = {
 	on_attach = on_attach,
 	capabilities = require('cmp_nvim_lsp').default_capabilities(),
 }
-for _, name in ipairs { 'pyright', 'rust_analyzer', 'tsserver', 'sumneko_lua' } do
-	lspconfig[name].setup(common_setup_data)
+for _, name in ipairs { 'pyright', 'rust_analyzer', 'tsserver' } do
+	setup(name, common_setup_data)
 end
 
 -- Setup sumneko server for Neovim lua.
@@ -44,7 +68,7 @@ do
 	table.insert(runtime_path, 'lua/?.lua')
 	table.insert(runtime_path, 'lua/?/init.lua')
 
-	lspconfig.sumneko_lua.setup {
+	setup('sumneko_lua', {
 		unpack(common_setup_data),
 		settings = {
 			Lua = {
@@ -66,5 +90,5 @@ do
 				},
 			},
 		},
-	}
+	})
 end
