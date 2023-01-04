fc2b827fab4aeac62e2d68ddc52a4b9bc726581d
Michon van Dooren <me@maienm.com>
2023-01-04 01:30:19 +0100

    Tweak indent-blankline style

---
+++ vim/rc/plugins/indent-blankline.lua
@@ -0,0 +1,16 @@
+local function theme_hook()
+	vim.cmd('highlight IndentBlanklineCharNormal guifg=#' .. vim.g.base16_gui01 .. ' gui=nocombine')
+	vim.cmd('highlight IndentBlanklineCharCurrent guifg=#' .. vim.g.base16_gui0D .. ' gui=nocombine')
+end
+
+vim.fn.RegisterThemeHook(theme_hook)
+
+require('indent_blankline').setup {
+	buftype_exclude = { 'terminal' },
+
+	char = '🭱',
+	show_current_context = true,
+
+	char_highlight_list = { 'IndentBlanklineCharNormal' },
+	context_highlight_list = { 'IndentBlanklineCharCurrent' },
+}
