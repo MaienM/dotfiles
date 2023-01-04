d97febbb97999eca767a741ce132e49e1f93cbf9
Michon van Dooren <me@maienm.com>
2023-01-04 23:24:23 +0100

    Add nice inc-rename popup to noice

--- vim/rc/plugins/noice.lua
+++ vim/rc/plugins/noice.lua
@@ -1,45 +1,34 @@
-function on_resize()
-	local lines = vim.opt.lines._value
-	local line33 = math.floor(lines / 3)
+require('noice').setup {
+	lsp = {
+		override = {
+			['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+			['vim.lsp.util.stylize_markdown'] = true,
+			['cmp.entry.get_documentation'] = true,
+		},
+	},
 
-	require('noice').setup {
-		views = {
-			-- Like the command_palette preset, but moved down to a more comfortable position.
-			cmdline_popup = {
-				position = {
-					row = line33,
-					col = '50%',
-				},
+	views = {
+		cmdline_popup = {
+			position = {
+				row = "33%",
+				col = '50%',
 			},
-			popupmenu = {
-				position = {
-					row = line33 + 3,
-					col = '50%',
-				},
-				size = {
-					width = 60,
-					height = 'auto',
-					max_height = 15,
-				},
-				border = {
-					style = 'rounded',
-					padding = { 0, 1 },
-				},
-				win_options = {
-					winhighlight = { Normal = 'Normal', FloatBorder = 'NoiceCmdlinePopupBorder' },
+		},
+	},
+
+	cmdline = {
+		format = {
+			increname = {
+				pattern = '^:IncRename%s+',
+				icon = '',
+				opts = {
+					relative = 'cursor',
+					size = { min_width = 20 },
+					position = { row = -2, col = 0 },
+					buf_options = { filetype = 'text' },
+					border = { text = { top = ' Rename ' } },
 				},
 			},
 		},
-
-		lsp = {
-			override = {
-				['vim.lsp.util.convert_input_to_markdown_lines'] = true,
-				['vim.lsp.util.stylize_markdown'] = true,
-				['cmp.entry.get_documentation'] = true,
-			},
-		},
-	}
-end
-
-vim.api.nvim_create_autocmd('VimResized', { callback = on_resize })
-on_resize()
+	},
+}
