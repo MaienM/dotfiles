20ea35fda83a92a908b87eae87edd29ab9b1825e
Michon van Dooren <me@maienm.com>
2023-01-06 22:47:54 +0100

    Cleanup notify.lua

--- vim/rc/plugins/notify.lua
+++ vim/rc/plugins/notify.lua
@@ -1,16 +1,17 @@
-require('notify').setup {
+local notify = require('notify')
+notify.setup {
 	stages = 'slide',
 }
 
 -- Theme {{{
 
-function set_group_color(group, color)
+local function set_group_color(group, color)
 	vim.cmd('highlight Notify' .. group .. 'Border guifg=#' .. color)
 	vim.cmd('highlight Notify' .. group .. 'Icon guifg=#' .. color)
 	vim.cmd('highlight Notify' .. group .. 'Title guifg=#' .. color)
 end
 
-function theme_hook()
+local function theme_hook()
 	set_group_color('ERROR', vim.g.base16_gui08)
 	set_group_color('WARN', vim.g.base16_gui0A)
 	set_group_color('INFO', vim.g.base16_gui0C)
