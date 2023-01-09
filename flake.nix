40f0d0d3caae6835d90f46b16d1b5bc01a832185
Michon van Dooren <me@maienm.com>
2023-01-09 12:08:21 +0100

    Refactor to use flake-utils.lib.eachDefaultSytem
    This means eash configuration is now available for all systems. This
    doesn't doesn't always make sense, but it also doesn't ultimately
    matter. The resulting flake.nix is simpler and faster than the way I had
    it written before, which _does_ matter.

--- flake.nix
+++ flake.nix
@@ -14,81 +14,58 @@
     neovim.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { nixpkgs, flake-utils, darwin, home-manager, ... }@inputs:
+  outputs = { nixpkgs, flake-utils, darwin, home-manager, ... }@inputs: flake-utils.lib.eachDefaultSystem (system:
     let
-      # ( system -> something } -> { system = something }
-      eachDefaultSystemBySystem =
-        (func:
-          builtins.listToAttrs (
-            builtins.map
-              (system: {
-                name = system;
-                value = func system;
-              })
-              flake-utils.lib.defaultSystems
-          )
-        );
-      # { system = nixpkgs.${system} with unfree allowed }
-      pkgs-unfree = eachDefaultSystemBySystem (system:
-        import "${nixpkgs}" {
-          inherit system;
-          config.allowUnfree = true;
-        }
-      );
-      # { system = { name = inputs.${name}.packages.${system} } }
-      pkgs-inputs = eachDefaultSystemBySystem (system:
+      pkgs = nixpkgs.legacyPackages.${system};
+      pkgs-unfree = import "${nixpkgs}" {
+        inherit system;
+        config.allowUnfree = true;
+      };
+      pkgs-inputs = # { name = inputs.${name}.packages.${system} }
         builtins.mapAttrs
           (_: nixpkgs.lib.attrByPath [ "packages" system ] { })
-          inputs
-      );
-      # { system = ./nix/pkgs for that system }
-      pkgs-local = eachDefaultSystemBySystem (system:
-        import ./nix/pkgs {
-          inherit inputs system;
-          pkgs = nixpkgs.legacyPackages.${system};
-        }
-      );
+          inputs;
+      pkgs-local = import ./nix/pkgs {
+        inherit inputs system pkgs;
+      };
     in
     {
-      # dot-nixos-rebuild (which is an alias for nixos-rebuild --flake .#your-hostname).
-      nixosConfigurations = {
-        MICHON-PC = nixpkgs.lib.nixosSystem {
-          specialArgs = { inherit inputs; };
-          modules = [ ./nix/nixos/configuration.nix ];
-        };
-      };
-
-      # dot-darwin-rebuild (which is an alias for darwin-rebuild --flake .#your-hostname).
-      darwinConfigurations = {
-        MICHON-MACBOOK = darwin.lib.darwinSystem {
-          inherit inputs;
-          system = "aarch64-darwin";
-          modules = [ ./nix/darwin/configuration.nix ];
-        };
-      };
-
-      # dot-home-manager (which is an alias for home-manager --flake .#your-username@your-hostname).
-      homeConfigurations = {
-        "maienm@MICHON-PC" = home-manager.lib.homeManagerConfiguration {
-          pkgs = nixpkgs.legacyPackages.x86_64-linux;
-          extraSpecialArgs = {
-            inherit inputs;
-            pkgs-unfree = pkgs-unfree.x86_64-linux;
-            pkgs-inputs = pkgs-inputs.x86_64-linux;
-            pkgs-local = pkgs-local.x86_64-linux;
+      packages = {
+        # dot-nixos-rebuild (which is an alias for nixos-rebuild --flake .#your-hostname).
+        nixosConfigurations = {
+          MICHON-PC = nixpkgs.lib.nixosSystem {
+            specialArgs = { inherit inputs; };
+            modules = [ ./nix/nixos/configuration.nix ];
           };
-          modules = [ ./nix/home-manager/pc.nix ];
         };
-        "maienm@MICHON-MACBOOK" = home-manager.lib.homeManagerConfiguration {
-          pkgs = nixpkgs.legacyPackages.aarch64-darwin;
-          extraSpecialArgs = {
+
+        # dot-darwin-rebuild (which is an alias for darwin-rebuild --flake .#your-hostname).
+        darwinConfigurations = {
+          MICHON-MACBOOK = darwin.lib.darwinSystem {
             inherit inputs;
-            pkgs-unfree = pkgs-unfree.aarch64-darwin;
-            pkgs-inputs = pkgs-inputs.aarch64-darwin;
-            pkgs-local = pkgs-local.aarch64-darwin;
+            system = "aarch64-darwin";
+            modules = [ ./nix/darwin/configuration.nix ];
           };
-          modules = [ ./nix/home-manager/macbook.nix ];
+        };
+
+        # dot-home-manager (which is an alias for home-manager --flake .#your-username@your-hostname).
+        homeConfigurations = {
+          "maienm@MICHON-PC" = home-manager.lib.homeManagerConfiguration {
+            inherit pkgs;
+            extraSpecialArgs = {
+              inherit inputs pkgs-unfree pkgs-inputs pkgs-local;
+            };
+            modules = [ ./nix/home-manager/pc.nix ];
+          };
+          "maienm@MICHON-MACBOOK" = home-manager.lib.homeManagerConfiguration {
+            inherit pkgs;
+            extraSpecialArgs = {
+              inherit inputs pkgs-unfree pkgs-inputs pkgs-local;
+            };
+            modules = [ ./nix/home-manager/macbook.nix ];
+          };
         };
       };
-    };
+    }
+  );
 }
