aa7febb29d4799101a9a30dde54f794627a82773
Michon van Dooren <me@maienm.com>
2023-01-10 00:39:06 +0100

    Add treesitter-context plugin

---
+++ vim/rc/plugins/treesitter.lua
@@ -0,0 +1,22 @@
+require('nvim-treesitter.configs').setup {
+	highlight = {
+		enable = true,
+	},
+	incremental_selection = {
+		enable = true,
+	},
+	indent = {
+		enable = true,
+	},
+}
+
+vim.opt.foldmethod = 'expr'
+vim.opt.foldexpr = require('nvim-treesitter').foldexpr
+
+require('treesitter-context').setup()
+
+local function theme_hook()
+	vim.cmd('highlight TreesitterContext guibg=#' .. vim.g.base16_gui01)
+end
+
+vim.fn.RegisterThemeHook(theme_hook)
