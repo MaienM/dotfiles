2106a2cee625aa8577a4508c9ce317e123ee6dfc
Michon van Dooren <me@maienm.com>
2023-01-14 12:22:06 +0100

    Only setup nix aliases if the commands are present

--- zsh/rc/misc/nix.zsh
+++ zsh/rc/misc/nix.zsh
@@ -1,3 +1,9 @@
-alias dot-nixos-rebuild='sudo nixos-rebuild --flake "$HOME/dotfiles#$(hostname)"'
-alias dot-darwin-rebuild='darwin-rebuild --flake "$HOME/dotfiles#$(hostname)"'
-alias dot-home-manager='home-manager --flake "$HOME/dotfiles#${USER}@$(hostname)"'
+if [[ ${commands[nixos-rebuild]} ]]; then
+	alias dot-nixos-rebuild='sudo nixos-rebuild --flake "$HOME/dotfiles#$(hostname)"'
+fi
+if [[ ${commands[darwin-rebuild]} ]]; then
+	alias dot-darwin-rebuild='darwin-rebuild --flake "$HOME/dotfiles#$(hostname)"'
+fi
+if [[ ${commands[home-manager]} ]]; then
+	alias dot-home-manager='home-manager --flake "$HOME/dotfiles#${USER}@$(hostname)"'
+fi
