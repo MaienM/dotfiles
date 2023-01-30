54fe461e89f3de45e0d9d2546dc942e25361afe3
Michon van Dooren <me@maienm.com>
2023-01-30 17:39:24 +0100

    Add aliases for common nix-shell uses

--- zsh/rc/misc/nix.zsh
+++ zsh/rc/misc/nix.zsh
@@ -1,3 +1,7 @@
+if ! [[ ${commands[nix-shell]} ]]; then
+	return
+fi
+
 if [[ ${commands[nixos-rebuild]} ]]; then
 	alias dot-nixos-rebuild='sudo nixos-rebuild --flake "$HOME/dotfiles#$(hostname)"'
 fi
@@ -7,3 +11,8 @@ fi
 if [[ ${commands[home-manager]} ]]; then
 	alias dot-home-manager='home-manager --flake "$HOME/dotfiles#${USER}@$(hostname)"'
 fi
+
+alias ns='nix-shell --command zsh -p'
+nr() {
+	nix-shell --command "$1" -p "$1"
+}
