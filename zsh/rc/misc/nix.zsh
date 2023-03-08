800b8015ac4a1b78ba7ae18720cc6dad16a5fc85
Michon van Dooren <me@maienm.com>
2023-03-08 15:17:28 +0100

    Add function to push dotfiles to a remote machine

--- zsh/rc/misc/nix.zsh
+++ zsh/rc/misc/nix.zsh
@@ -52,3 +52,40 @@ nx() {
 	nix-shell --command "$(printf '%q ' "${prog##*.}" "$@")" -p "$prog"
 }
 
+dot-nix-push() {
+(
+	set -e
+	ssh "$1" '
+		set -e
+
+		cd dotfiles
+
+ 		if ! [ "$(git rev-parse --abbrev-ref HEAD)" = master ]; then
+			>&2 echo "Not on master branch, aborting."
+			exit 1
+		fi
+ 		if [ -n "$(git status --porcelain=v1 2>/dev/null)" ]; then
+			>&2 echo "There are uncomitted changes, aborting."
+			exit 1
+		fi
+		if ! [ "$(git rev-parse HEAD)" = "$(git rev-parse desktop/master)" ]; then
+			>&2 echo "There are local commits, aborting."
+			exit 1
+		fi
+
+		if ! git show-ref --verify --quiet refs/heads/desktop/master > /dev/null 2>&1; then
+			echo "There is no desktop/master branch, creating it now."
+			git checkout -b desktop/master
+			git checkout master
+		fi
+	'
+	git push -f "$1" master:desktop/master
+	ssh "$1" '
+		set -e
+
+		cd dotfiles
+		git checkout desktop/master -B master
+	'
+	echo 'Remote dotfiles have been updated.'
+)
+}
