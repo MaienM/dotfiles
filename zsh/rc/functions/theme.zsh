68200a1494d0eecd87f988d2b75ed6ef2fba37f6
Michon van Dooren <me@maienm.com>
2023-04-17 11:24:49 +0200

    Small fixes to base16 builder

--- zsh/rc/functions/theme.zsh
+++ zsh/rc/functions/theme.zsh
@@ -1,10 +1,10 @@
 function activate_base16_theme() {
-	setopt localoptions errexit
+	setopt localoptions errreturn
 	base16-colorscheme "$1"
 	source ~/.profile.d/50_base16.local
 	source ~/.profile.d/50_dircolors.local
 	source ~/.zsh/rc/base16.local.zsh
 }
 
-alias let-there-be-light='activate_base16_theme light || true'
-alias it-burns='activate_base16_theme dark || true'
+alias let-there-be-light='activate_base16_theme light'
+alias it-burns='activate_base16_theme dark'
