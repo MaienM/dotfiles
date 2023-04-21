688024966c9716b288f98eb4734c4be53b4b3eaf
Michon van Dooren <me@maienm.com>
2023-04-21 13:03:48 +0200

    Fix tmux save strategy on MacOS

--- tmux/save-command-strategy--ps-foreground.sh
+++ tmux/save-command-strategy--ps-foreground.sh
@@ -8,11 +8,21 @@ exit_safely_if_empty_ppid() {
 	fi
 }
 
+get_child_pids() {
+	ps -awwo ppid=,pid= \
+		| awk -v ppid="$1" '{ if ($1 == ppid) print $2 }' \
+		| while read -r pid; do
+			printf '%s\n' "$pid"
+			get_child_pids "$pid"
+		done
+}
+
 full_command() {
-	ps --forest -g "$PANE_PID" -o pid,stat \
-		| grep '+$' \
+	get_child_pids "$PANE_PID" \
+		| xargs --no-run-if-empty ps -wwo pid=,stat= -p \
+		| grep '+\s*$' \
 		| awk '{ print $1 }' \
-		| xargs --no-run-if-empty ps -o cmd --no-headers \
+		| xargs --no-run-if-empty ps -wwo command= \
 		| tail -n1
 }
 
