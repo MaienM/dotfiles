ea4412a0505332be10d8d0013a10657bf8115ca9
Michon van Dooren <me@maienm.com>
2023-04-24 21:04:32 +0200

    Add script to display ZFS ARC hitrates

---
+++ scripts/misc/arc-hitrates.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+head -n12 /proc/spl/kstat/zfs/arcstats \
+| tail -n10 \
+| while read -r hitline; read -r missesline; do
+	printf '%s\n' "$hitline" "$missesline"
+
+	prefix="${hitline%hits *}"
+	if [[ "$prefix" == "$hitline" ]] || [[ "$missesline" != "${prefix}misses "* ]] ; then
+		echo >&2 "Unable to parse."
+		exit 1
+	fi
+
+	hits="${hitline##* }"
+	misses="${missesline##* }"
+	printf '%shitrate - %f%%\n' "${prefix}" "$(calc "$hits/($hits+$misses)*100")"
+done \
+| column -t
