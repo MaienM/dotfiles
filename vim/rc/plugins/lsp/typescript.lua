3d55e0880f1deeabd80ee12ea2b634d56882641e
Michon van Dooren <me@maienm.com>
2023-04-24 21:42:32 +0200

    Setup eslint_d for JS/TS

--- vim/rc/plugins/lsp/typescript.lua
+++ vim/rc/plugins/lsp/typescript.lua
@@ -1,1 +1,6 @@
 vim.g.mylsp.setup('tsserver')
+
+local null_ls = require('null-ls')
+null_ls.register(null_ls.builtins.code_actions.eslint_d)
+null_ls.register(null_ls.builtins.diagnostics.eslint_d)
+null_ls.register(null_ls.builtins.formatting.eslint_d)
