bd8ef109c0e00294e8c9a316c4175a4a98540b81
Michon van Dooren <me@maienm.com>
2023-09-19 21:24:09 +0200

    Setup rust-tools.nvim

--- vim/rc/plugins/lsp/rust.lua
+++ vim/rc/plugins/lsp/rust.lua
@@ -1,1 +1,9 @@
-vim.g.mylsp.setup('rust_analyzer')
+local rt = require('rust-tools')
+
+rt.setup {
+	server = {
+		on_attach = function(client, bufnr)
+			vim.g.mylsp.on_attach(client, bufnr)
+		end,
+	},
+}
