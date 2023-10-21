7f8df80da01bf88e1e572609b7bcd36b241c66d4
Michon van Dooren <me@maienm.com>
2023-10-21 14:39:33 +0200

    Simplify theme overrides in neovim

--- vim/rc/general/look-and-feel.vim
+++ vim/rc/general/look-and-feel.vim
@@ -10,18 +10,12 @@ endif
 colorscheme base16
 
 " Don't set a background using the theme, falling back on the color set by the terminal. This allows for transparency.
-au ColorScheme * hi Normal ctermbg=none
-au ColorScheme * hi NonText ctermbg=none
+au ColorScheme,VimEnter * hi Normal ctermbg=none
+au ColorScheme,VimEnter * hi NonText ctermbg=none
 
 " Customize the look of NeoVim floating windows.
 au FileType fzf setlocal nonumber norelativenumber
-au ColorScheme * hi NormalFloat ctermbg=18
+au ColorScheme,VimEnter * hi NormalFloat ctermbg=18
 
 " Set the font for gvim.
 set guifont="Fira Code:h11"
-
-" Prepare a list of functions to execute when the theme has been (re)loaded.
-let g:theme_hooks = []
-function! RegisterThemeHook(func)
-	call add(g:theme_hooks, a:func)
-endfunction
--- vim/rc/plugins/indent-blankline.lua
+++ vim/rc/plugins/indent-blankline.lua
@@ -1,11 +1,3 @@
-local function theme_hook()
-	local colors = require('base16-colorscheme').colors
-	vim.cmd('highlight IndentBlanklineCharNormal guifg=' .. colors.base01 .. ' gui=nocombine')
-	vim.cmd('highlight IndentBlanklineCharCurrent guifg=' .. colors.base0D .. ' gui=nocombine')
-end
-
-vim.fn.RegisterThemeHook(theme_hook)
-
 require('indent_blankline').setup {
 	buftype_exclude = {
 		'nofile',
@@ -18,3 +10,11 @@ require('indent_blankline').setup {
 	char_highlight_list = { 'IndentBlanklineCharNormal' },
 	context_highlight_list = { 'IndentBlanklineCharCurrent' },
 }
+
+vim.api.nvim_create_autocmd({ 'VimEnter', 'ColorScheme' }, {
+	callback = function()
+		local colors = require('base16-colorscheme').colors
+		vim.cmd('highlight IndentBlanklineCharNormal guifg=' .. colors.base01 .. ' gui=nocombine')
+		vim.cmd('highlight IndentBlanklineCharCurrent guifg=' .. colors.base0D .. ' gui=nocombine')
+	end,
+})
