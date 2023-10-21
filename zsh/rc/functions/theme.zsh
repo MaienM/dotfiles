914cfbca93502cd76afd61aa94ed0d1af1718b34
Michon van Dooren <me@maienm.com>
2023-10-21 15:09:30 +0200

    Allow arguments to base16-colorscheme in aliases

--- zsh/rc/functions/theme.zsh
+++ zsh/rc/functions/theme.zsh
@@ -1,10 +1,10 @@
-function activate_base16_theme() {
+function base16-colorscheme() {
 	setopt localoptions errreturn
-	base16-colorscheme "$1"
+	command base16-colorscheme "$@"
 	source ~/.profile.d/50_base16.local
 	source ~/.profile.d/50_dircolors.local
 	source ~/.zsh/rc/base16.local.zsh
 }
 
-alias let-there-be-light='activate_base16_theme light'
-alias it-burns='activate_base16_theme dark'
+alias let-there-be-light='base16-colorscheme light'
+alias it-burns='base16-colorscheme dark'
