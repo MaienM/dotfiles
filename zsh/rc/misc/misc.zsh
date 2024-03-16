8837d54876e9fd1f5f08c3fe801f24bb6467c211
Michon van Dooren <me@maienm.com>
2024-03-16 14:48:31 +0100

    Add functions to transform clipboard

--- zsh/rc/misc/misc.zsh
+++ zsh/rc/misc/misc.zsh
@@ -15,11 +15,20 @@ fi
 
 if [[ $commands[xclip] ]]; then
 	# x selection to clipboard (and vice versa).
-	alias xsc='xclip -out | xclip -selection Clipboard'
-	alias xcs='xclip -out -selection Clipboard | xclip'
+	alias xsc='xclip -out | xclip -in -selection Clipboard'
+	alias xcs='xclip -out -selection Clipboard | xclip -in'
+
+	# transform clipboard contents.
+	function xst() {
+		xclip -out | "$@" | xclip -in
+	}
+	function xct() {
+		xclip -out -selection clipboard | "$@" | xclip -in -selection clipboard
+	}
 fi
 
 if [[ $commands[xprop] ]]; then
 	# xprop, stripping the icon from the output
 	alias xpropni="xprop | awk -vskip=0 '{ if (/^\S/) { skip = 0 }; if (/^_NET_WM_ICON/) { skip = 1 }; if (skip != 1) { print } }'"
 fi
+
