04b085095b3df844213a9f58d40b4ca14afeba35
Michon van Dooren <me@maienm.com>
2024-03-16 14:18:27 +0100

    Allow flashing QMK boards without root

--- nix/nixos/common/default.nix
+++ nix/nixos/common/default.nix
@@ -120,6 +120,9 @@ in
     dockerCompat = true;
   };
 
+  # Rootless flashing of QMK boards.
+  hardware.keyboard.qmk.enable = true;
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
