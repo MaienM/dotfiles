1fd7c8ceff481d394711cbde6a62d144341ffbbc
Michon van Dooren <me@maienm.com>
2024-04-16 01:10:24 +0200

    Only enable dprint LSP if the binary is present
    This binary being absent is not handled gracefully and results in a
    error every time you switch to a buffer that would use the dprint LSP.
    We could just suppress this message, but as the binary won't generally
    magically appear mid-session a single check at startup seems cleaner.

--- vim/rc/plugins/lsp/general.lua
+++ vim/rc/plugins/lsp/general.lua
@@ -1,10 +1,12 @@
 local null_ls = require('null-ls')
 null_ls.register(null_ls.builtins.diagnostics.editorconfig_checker)
 
-vim.g.mylsp.setup('dprint', {
-	filetypes = {
-		'jsonc',
-		'json5',
-		unpack(require('lspconfig.server_configurations.dprint').default_config.filetypes),
-	},
-})
+if next(vim.fn['xolox#misc#path#which']('dprint')) ~= nil then
+	vim.g.mylsp.setup('dprint', {
+		filetypes = {
+			'jsonc',
+			'json5',
+			unpack(require('lspconfig.server_configurations.dprint').default_config.filetypes),
+		},
+	})
+end
