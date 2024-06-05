06d205b7d70f01b299f8f8b1b3f73c61293d1e82
Michon van Dooren <me@maienm.com>
2024-06-05 10:12:21 +0200

    Update nvim-trouble

--- vim/rc/plugins/trouble.lua
+++ vim/rc/plugins/trouble.lua
@@ -4,13 +4,13 @@ trouble.setup {}
 
 vim.keymap.set('n', ']D', function()
 	trouble.open()
-	trouble.next { skip_groups = true, jump = true }
+	trouble.next('diagnostics', { skip_groups = true, jump = true })
 end, {
 	desc = 'Next item in Trouble list',
 })
 vim.keymap.set('n', '[D', function()
 	trouble.open()
-	trouble.previous { skip_groups = true, jump = true }
+	trouble.prev('diagnostics', { skip_groups = true, jump = true })
 end, {
 	desc = 'Previous item in Trouble list',
 })
