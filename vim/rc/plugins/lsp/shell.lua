4ec965d5306ada60a0ba7e48645b954dc8b8f7bf
Michon van Dooren <me@maienm.com>
2024-08-17 16:09:59 +0200

    Move shfmt from null-ls to bashls

--- vim/rc/plugins/lsp/shell.lua
+++ vim/rc/plugins/lsp/shell.lua
@@ -6,15 +6,18 @@ vim.g.mylsp.setup('bashls', {
 			-- 2) Starting this isn't automated yet, which kinda sucks. Perhaps once home-manager has support for oci containers it can be managed using this.
 			-- podman run --rm -d -e HOST_IP=0.0.0.0 -p 30608:5000 ghcr.io/idank/idank/explainshell:sha-0b660cf@sha256:2691b4caf68d062bcf66b0c87241bca52ec2c2bfa57049ed4678a90b2b0f417d
 			-- explainshellEndpoint = 'http://localhost:30608/',
+
+			shfmt = {
+				binaryNextLine = true,
+				caseIndent = true,
+				spaceRedirects = true,
+			},
 		},
 	},
 })
 
 local null_ls = require('null-ls')
 null_ls.register(null_ls.builtins.diagnostics.zsh)
-null_ls.register(null_ls.builtins.formatting.shfmt.with {
-	extra_args = { '-bn', '-ci', '-sr' },
-})
 
 -- bashls integrates shellcheck, but it doesn't offer an action to ignore an error yet.
 null_ls.register(require('none-ls-shellcheck.code_actions'))
