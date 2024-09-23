869d03b4869702c639fec0c60e9a4a384c2f1415
Michon van Dooren <me@maienm.com>
2024-09-23 19:22:27 +0200

    Directly use state file for pulumi fzf pipeline
    This is not elegant, but neither was the parsing of the human-readable
    tree that was used in the previous implementation. It is a lot simpler
    and faster though.

--- zsh/rc/fzf/pulumi.zsh
+++ zsh/rc/fzf/pulumi.zsh
@@ -1,66 +1,19 @@
 # stack
 _fzf_pipeline_pulumi_stack_urn_source() {
-	local i line lines patterns type name attributes info
-
-	i=0
-
-	lines=()
-	while read -r line; do
-		lines=("${lines[@]}" "$line")
-	done < <(pulumi stack -ui)
-
-	patterns=(
-		resource '\s*[├└]─\s*(\S+)\s+(\S+)$'
-		attribute '\s*│\s*(\S+):\s+(\S+)$'
-	)
-
-	parse_line() {
-		local name pattern
-		for name pattern in "${patterns[@]}"; do
-			if [[ "$1" =~ "$pattern" ]]; then
-				line=("$name" "${match[@]}")
-				return 0
-			fi
-		done
+	local match stack
+	if ! [[ "$(pulumi config get doesnotexist 2>&1 || true)" =~ "^error: configuration key 'doesnotexist' not found for stack '(.*)'$" ]]; then
+		>&2 echo "Cannot determine current pulumi stack."
 		return 1
-	}
+	fi
+	stack="${match[1]}"
 
-	peek_next() {
-		while [ "$i" -lt "${#lines[@]}" ]; do
-			if ! parse_line "${lines["$i"]}"; then
-				: $((i+=1))
-				continue
-			fi
-			return 0
-		done
-		return 1
-	}
-
-	next() {
-		peek_next
-		: $((i+=1))
-	}
-
-	# Skip until the start of the first resource.
-	while peek_next && [ "${line[1]}" != 'resource' ]; do
-		next
-	done
-
-	while peek_next; do
-		next
-		type="${line[2]}"
-		name="${line[3]}"
-
-		declare -A attributes
-		while peek_next && [ "${line[1]}" != 'resource' ]; do
-			next
-			attributes[${line[2]}]="${line[3]}"
-		done
-		[ -n "${attributes[URN]}" ] || continue
-
-		info="${(q)attributes[URN]} ${(q)attributes[ID]} ${(q)type} ${(q)name}"
-		echo "${(q)info} ${type} ${name}"
-	done
+	jq -r '
+		.checkpoint | (.latest // .Latest) | .resources[] |
+		.name = (.urn | split("::") | last) |
+		.info = @sh "\(.urn) \(.id) \(.type) \(.name)" |
+		"\(.info | @sh) \(.type) \(.name)"
+	' \
+	< "$HOME/.pulumi/stacks/$stack.json"
 }
 _fzf_pipeline_pulumi_stack_urn_preview() {
 	info=("${(z)${(Q)1}}")
@@ -87,10 +40,6 @@ _fzf_pipeline_pulumi_stack_type_and_name_target() {
 	info=("${(z)${(Q)1}}")
 	echo "${(Q)info[3]} ${(Q)info[4]}"
 }
-
-if ! command -v pulumi &> /dev/null; then
-	return
-fi
 
 # Presets
 alias _fzf_preset_pulumi_stack_urn='_fzf_config_add pulumi_stack_urn'
